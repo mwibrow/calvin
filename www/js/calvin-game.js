39c5c9b4591c6ca964f0f08f13a250c04b74b62d
Mark Wibrow <m.wibrow@gmail.com>
2017-01-30 14:11:10 +0000

    Initial commit

---
+++ www/js/calvin-game.js
@@ -0,0 +1,41 @@
+var Themes = {
+    Bee: {
+        path: "bee",
+        cast: [
+            {
+                id: "background",
+                src: "background.png",
+            },
+            {
+                id: "backgroundItems",
+                src: "cloud.png",
+                count: 2
+            },
+            {
+                id: "postBackground",
+                src: "hills.png",
+                count: 4
+            },
+            {
+                id: "preForeground",
+                src: "hedges.png",
+                count: 4
+            },
+            {
+                id: "target",
+                src: "spritesheet_flowers.png"
+            },
+            {
+                id: "player",
+                src: "spritesheet_bee.png"
+            }
+        ]
+    }
+}
+
+
+function Game(canvasId, theme) {
+
+
+
+}
