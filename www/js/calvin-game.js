9841e6f02142636bea2d9ce1528e8ef7cfaff5e3
Mark Wibrow <m.wibrow@gmail.com>
2017-02-04 17:05:43 +0000

    Added targets and player

--- www/js/calvin-game.js
+++ www/js/calvin-game.js
@@ -1,50 +1,6 @@
-var Themes = {
-    Bee: {
-        path: "Bee",
-        cast: [
-            {
-                id: "background",
-                src: "background.png",
-            },
-            {
-                id: "backgroundItem",
-                src: "cloud.png",
-                count: 2
-            },
-            {
-                id: "postBackground",
-                src: "hills.png",
-                frameCount: 4
-            },
-            {
-                id: "preForeground",
-                src: "hedges.png",
-                marginX: 0,
-                marginY: 0,
-                count: 4
-            },
-            {
-                id: "target",
-                src: "spritesheet_flowers.png"
-            },
-            {
-                id: "player",
-                src: "spritesheet_bee.png"
-            }
-        ]
-    }
-}
-
 
 function Game(canvasId, properties) {
-    this.properties = {
-        theme: Themes.Bee
-    };
-    for (property in properties) {
-        if (properties.hasOwnProperty(property)) {
-            this.properties[property] = properties[property];
-        }
-    }
+
     createjs.Stage.prototype.snapToPixel = true;
 
 
@@ -52,13 +8,12 @@ function Game(canvasId, properties) {
     this.stage.snapToPixelEnabled = true;
     this.stageWidth = this.stage.canvas.width;
     this.stageHeight = this.stage.canvas.height;
-
+    this.interTargetDuration = 3000;
     this._initialised = false;
 }
 
 Game.prototype.initialise = function() {
-    this.theme = Themes[this.properties.theme];
-    this.themeIndex = {};
+
     this.cast = {
         background: null,
         backgroundItems: [],
@@ -66,35 +21,34 @@ Game.prototype.initialise = function() {
         preForegroundItems: [],
         targets: [],
         player: null
-    };
-
-    this.manifest = [];
-    for (var i = 0; i < this.theme.cast.length; i ++) {
-        this.manifest.push({src: this.theme.cast[i].src,
-                            id: this.theme.cast[i].id});
-        this.themeIndex[this.theme.cast[i].id] = i;
     }
+
+    this.manifest = [
+        {src: "background.png", id: "background"},
+        {src: "cloud.png", id: "backgroundItem"},
+        {src: "hills.png", id: "postBackground"},
+        {src: "hedges.png", id: "preForeground"},
+        {src: "spritesheet_bee.png", id: "player"},
+        {src: "spritesheet_flowers.png", id: "target"}
+    ]
 
     var that = this;
     this.loader = new createjs.LoadQueue(false);
     this.loader.addEventListener("complete", function() { that._initialise(); });
     this.loader.loadManifest(this.manifest, true,
-        'img/game/themes/' + this.theme.path + '/');
+        'img/game/themes/' + "Bee"+ '/');
 }
 
 Game.prototype._initialise = function() {
     var i, j, k, bitmap, item, themeItem, spritesheet, scaleX, scaleY;
 
     // Background
-    this.cast.background = new createjs.Shape();
     bitmap = new createjs.Bitmap(this.loader.getResult("background"));
-    scaleY = this.stageHeight / bitmap.image.height;
-    this.cast.background.graphics
-        .beginBitmapFill(bitmap.image,
-                         "repeat",
-                         new createjs.Matrix2D(1, 0, 0, scaleY, 0, 0))
-        .drawRect(0, 0, this.stageWidth, this.stageHeight);
-    this.stage.addChild(this.cast.background);
+    bitmap.x = bitmap.y = 0;
+    bitmap.scaleX = this.stage.canvas.width / bitmap.image.width;
+    bitmap.scaleY = this.stage.canvas.height / bitmap.image.height;
+    this.cast.background = bitmap;
+    this.stage.addChild(bitmap);
 
     // BackgroundItems
     for (var i = 0; i < 2; i ++) {
@@ -106,12 +60,12 @@ Game.prototype._initialise = function() {
         bitmap.width = bitmap.image.width;
         bitmap.height = bitmap.image.height;
         bitmap.alpha = 0.5 +  i / 2;
+        bitmap.deltaScaleX = 4 + i * 2;
         this.cast.backgroundItems.push(bitmap);
         this.stage.addChild(bitmap);
     }
 
     // Post-background
-    themeItem = this.theme.cast[this.themeIndex.postBackground];
     bitmap = new createjs.Bitmap(this.loader.getResult("postBackground"));
     spriteSheet = new createjs.SpriteSheet({
         framerate: 1,
@@ -119,9 +73,9 @@ Game.prototype._initialise = function() {
         "frames": {
             "regX": 0,
             "regY": 0,
-            "width": bitmap.image.width / themeItem.frameCount,
+            "width": bitmap.image.width / 4,
             "height": bitmap.image.height,
-            "count": themeItem.count
+            "count": 4
         },
         "animations": {
             "frame1": [0, 0, "frame1"],
@@ -131,11 +85,11 @@ Game.prototype._initialise = function() {
         }
     });
 
-    var k = Math.floor(this.stage.canvas.width / bitmap.image.width * themeItem.frameCount) + 2;
+    var k = Math.floor(this.stage.canvas.width / bitmap.image.width * 4) + 2;
     for (i = 0; i < k; i ++) {
-        j = Math.floor(Math.random() * themeItem.frameCount) + 1;
+        j = Math.floor(Math.random() * 4) + 1;
         item = new createjs.Sprite(spriteSheet, "frame" + j);
-        item.width = bitmap.image.width / themeItem.frameCount;
+        item.width = bitmap.image.width / 4;
         item.height = bitmap.image.height;
         item.x =  item.width * i;
         item.y = (this.stageHeight / 2) - item.height * 15 / 16;
@@ -143,7 +97,7 @@ Game.prototype._initialise = function() {
         this.stage.addChild(item);
     }
 
-    themeItem = this.theme.cast[this.themeIndex.preForeground];
+
     bitmap = new createjs.Bitmap(this.loader.getResult("preForeground"));
     spriteSheet = new createjs.SpriteSheet({
         framerate: 1,
@@ -151,7 +105,7 @@ Game.prototype._initialise = function() {
         "frames": {
             "regX": 0,
             "regY": 0,
-            "width": bitmap.image.width / themeItem.count,
+            "width": bitmap.image.width / 4,
             "height": bitmap.image.height,
             "count": 4
         },
@@ -163,14 +117,41 @@ Game.prototype._initialise = function() {
         }
     });
     for (i = 0; i < 4; i ++) {
-        j = Math.floor(Math.random() * themeItem.count) + 1;
+        j = Math.floor(Math.random() * 4) + 1;
         item = new createjs.Sprite(spriteSheet, "frame" + j);
-        item.width = bitmap.image.width / themeItem.count;
-        item.x =  bitmap.image.width / themeItem.count * i;
+        item.width = bitmap.image.width / 4;
+        item.x =  bitmap.image.width / 4 * i;
         item.y = (this.stageHeight * 5 / 8) - bitmap.image.height * 15 / 16;
         this.cast.preForegroundItems.push(item);
         this.stage.addChild(item);
     }
+
+
+
+    bitmap = new createjs.Bitmap(this.loader.getResult("player"));
+    spriteSheet = new createjs.SpriteSheet({
+        framerate: 5,
+        "images": [bitmap.image],
+        "frames": {
+            "regX": 0,
+            "regY": 0,
+            "width": bitmap.image.width / 2,
+            "height": bitmap.image.height,
+            "count": 2
+        },
+        "animations": {
+            "frame1": [0, 1, "frame1"]
+        }
+    });
+    item = new createjs.Sprite(spriteSheet, "frame1");
+    item.x = this.stage.canvas.width / 3;
+    item.y = this.stage.canvas.height / 2;
+    item.regX = bitmap.image.width / 4;
+    item.regY = bitmap.image.height / 2;
+    this.stage.addChild(item);
+    this.cast.player = item;
+
+    this.addTarget();
 
     createjs.Ticker.timingMode = createjs.Ticker.RAF;
     var that = this;
@@ -178,9 +159,41 @@ Game.prototype._initialise = function() {
 
 }
 
+Game.prototype.addTarget = function() {
+    var item, bitmap, spritesheet, i;
+    bitmap = new createjs.Bitmap(this.loader.getResult("target"));
+    spriteSheet = new createjs.SpriteSheet({
+        framerate: 1,
+        "images": [bitmap.image],
+        "frames": {
+            "regX": 0,
+            "regY": 0,
+            "width": bitmap.image.width / 2,
+            "height": bitmap.image.height,
+            "count": 2
+        },
+        "animations": {
+            "frame1": [0, 0, "frame1"],
+            "frame2": [1, 1, "frame2"],
+        }
+    });
+    item = new createjs.Sprite(spriteSheet, "frame2");
+    item.x = this.stage.canvas.width + bitmap.image.width / 2;
+    item.y = Math.random() * (this.stage.canvas.height - 2 * bitmap.image.height) + bitmap.image.height;
+    item.regX = bitmap.image.width / 4;
+    item.regY = bitmap.image.height / 4;
+    i = this.stage.getChildIndex(this.cast.player);
+    item.index = i;
+    item.collected = false;
+    this.cast.targets.push(item);
+    this.stage.addChildAt(item, i);
+
+    var that = this;
+    window.setTimeout(function(){ that.addTarget() }, that.interTargetDuration);
+}
 Game.prototype.tick = function(event) {
     var i, j, item;
-    var deltaX = event.delta / (300 - 0);
+    var deltaX = event.delta / (100 - 0);
 
     for (i = 0; i < this.cast.backgroundItems.length; i ++) {
         item = this.cast.backgroundItems[i];
@@ -211,5 +224,26 @@ Game.prototype.tick = function(event) {
         }
     }
 
+    var target, targetX = this.stage.canvas.width, targetY = 0;
+    for (i = 0; i < this.cast.targets.length; i ++) {
+        item = this.cast.targets[i];
+        item.x = item.x - deltaX * 20;
+        if ((item.x < this.cast.player.x) && !item.collected) {
+            item.collected = true;
+            createjs.Tween.get(item).to({scaleX: 0, scaleY: 0},
+             500, createjs.Ease.backIn);
+        }
+        if (item.x + item.width * item.scaleX <= 0) {
+            this.stage.removeChild(item);
+        }
+        j = item.x - this.cast.player.x;
+        if ((j > 0) && (j < targetX)) {
+            targetX = j;
+            targetY = item.y - this.cast.player.y;
+        }
+    }
+    if (targetY !== 0) {
+        this.cast.player.y = this.cast.player.y + targetY / 32;
+    }
     this.stage.update(event);
 }
