81ef061242e9db213d4bfbcdf2af96005a8438d6
mwibrow <m.wibrow@gmail.com>
2017-07-15 10:51:23 +0100

    Singleton Animation avoiding multiple calls to window.requestAnimationFrame

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,68 @@ import { VowelTrainerPage } from '../pages/vowel-trainer/vowel-trainer';
 })
 export class MyApp {
   rootPage:any = VowelTrainerPage;
-
+  animation: Animation;
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.animation = Animation.getInstance();
+      this.animation.start();
     });
   }
 }
 
+
+class Animation {
+
+  private static instance: Animation = new Animation();
+  private callbacks: any;
+  private running: boolean;
+
+  private constructor() {
+    this.callbacks = {};
+    this.running = false;
+  }
+
+  public static getInstance() {
+    return Animation.instance;
+  }
+
+  public isRunning() {
+    return this.running;
+  }
+
+  public addCallback(callback:any, name:string) {
+    this.callbacks[name] = callback;
+  }
+
+  public deleteCallback(name:string) {
+     if (this.callbacks.hasOwnProperty(name)) {
+        delete this.callbacks[name];
+      }
+  }
+
+  private doCallbacks(me: Animation, timestamp: any) {
+    let name: string;
+    for (name in this.callbacks) {
+      if (this.callbacks.hasOwnProperty(name)) {
+        this.callbacks[name](this, timestamp);
+      }
+    }
+    if (me.running) {
+      window.requestAnimationFrame((timestamp) => me.doCallbacks(me, timestamp));
+    }
+  }
+
+  public start() {
+    this.running = true;
+    window.requestAnimationFrame((timestamp) => this.doCallbacks(this, timestamp));
+  }
+
+  public stop() {
+    this.running = false;
+  }
+}
