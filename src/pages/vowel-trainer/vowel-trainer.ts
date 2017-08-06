5690edadd6d06a4444733996d281e22fbd8bf4dd
mwibrow <m.wibrow@gmail.com>
2017-08-06 08:06:05 +0100

    Some upgrades

--- src/pages/vowel-trainer/vowel-trainer.ts
+++ src/pages/vowel-trainer/vowel-trainer.ts
@@ -12,6 +12,8 @@ import { KeywordComponent } from '../../components/keyword/keyword';
  * on Ionic pages and navigation.
  */
 
+declare var fs;
+
 enum ViewState {
   Audio,
   Video,
@@ -51,6 +53,13 @@ export class VowelTrainerPage {
     this.keywordExamples = appData.keywordExamples;
     this.player = this.audioProvider.getAudioPlayer();
 
+    fs.lstat('results', (err, stats) => {
+      if (!stats.isDirectory()) {
+        fs.mkdir('results', 0o777, (err) => {
+          console.log(err)
+        });
+      }
+    });
   }
 
 
