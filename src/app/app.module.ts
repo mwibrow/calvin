ca17550930f741579899b89e13b862522d19b911
mwibrow <m.wibrow@gmail.com>
2017-10-01 19:42:26 +0100

    exit modal ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { CalvinApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { ExitAppPage } from '../pages/exit-app/exit-app';
 import { CalvinLogoComponent } from '../components/calvin-logo/calvin-logo';
 import { AudioProvider } from '../providers/audio/audio';
 import { AppDataProvider } from '../providers/app-data/app-data';
+
 @NgModule({
   declarations: [
     CalvinApp,
     HomePage,
-    CalvinLogoComponent
+    CalvinLogoComponent,
+    ExitAppPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import { AppDataProvider } from '../providers/app-data/app-data';
   bootstrap: [IonicApp],
   entryComponents: [
     CalvinApp,
-    HomePage
+    HomePage,
+    ExitAppPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component, HostListener } from '@angular/core';
-import { NavController, App } from 'ionic-angular';
+import { NavController, App, ModalController, AlertController } from 'ionic-angular';
 import { AudioProvider } from '../../providers/audio/audio';
-
+import { ExitAppPage } from '../exit-app/exit-app';
 const remote = window['require']('electron').remote;
 
 
@@ -19,6 +19,8 @@ export class HomePage {
   constructor(
     private audio: AudioProvider,
     public navCtrl: NavController,
+    public modalCtrl: ModalController,
+    public alertCtrl: AlertController,
     private app: App) {
   }
 
@@ -40,7 +42,16 @@ export class HomePage {
   }
 
   exitApplication() {
-    remote.getCurrentWindow().close();
+    //remote.getCurrentWindow().close();
+    // const profileModal = this.modalCtrl.create(ExitAppPage, { userId: 8675309 });
+    // profileModal.present();
+    let alert = this.alertCtrl.create({
+      title: '<span>Quit?</span?',
+      subTitle: 'Are you sure you want to quit <span class="logo-c">C</span>ALVin?',
+      buttons: ['Cancel', 'OK'],
+      cssClass: 'alert'
+    });
+    alert.present();
   }
 
   handleKeyboardEvents(event) {
@@ -63,3 +74,4 @@ export class HomePage {
 
 
 }
+
