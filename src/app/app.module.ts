3a3efb12f47354be99420b15995e8bdb05c14491
Mark Wibrow <m.wibrow@gmail.com>
2017-10-03 12:13:27 +0100

    Updated app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { CalvinApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { VowelTrainerPage } from '../pages/vowel-trainer/vowel-trainer';
+import { KeywordTrainerPage } from '../pages/keyword-trainer/keyword-trainer';
+import { SelectTalkerPage } from '../pages/select-talker/select-talker';
 import { AudioProvider } from '../providers/audio/audio';
 import { AppDataProvider } from '../providers/app-data/app-data';
 
 @NgModule({
   declarations: [
     CalvinApp,
-    HomePage
+    HomePage,
+    VowelTrainerPage,
+    KeywordTrainerPage
   ],
   imports: [
     BrowserModule,
