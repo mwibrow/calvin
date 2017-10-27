f0a679d1b9683201d459118d79c99f253f9ddb11
mwibrow <m.wibrow@gmail.com>
2017-10-27 18:31:31 +0100

    Updated merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,7 @@ import { ExampleWordPage } from '../pages/example-word/example-word';
   templateUrl: 'app.html'
 })
 export class CalvinApp {
-<<<<<<< HEAD
-  rootPage:any = HomePage;
-=======
   rootPage:any = ExampleWordPage;
->>>>>>> afba3baf9ff6fe271df91acaf6b1ce5858c6bfcf
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
