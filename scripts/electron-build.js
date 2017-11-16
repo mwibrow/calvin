8e76b799e5e358d32321e59718e419ccd37142a6
Mark Wibrow <m.wibrow@gmail.com>
2017-11-16 09:01:47 +0000

    fiddled with build script

--- scripts/electron-build.js
+++ scripts/electron-build.js
@@ -5,6 +5,7 @@ const argv = require('minimist')(process.argv.slice(1));
 const args = Object.keys(argv)
     .filter(key => key !== '_')
     .reduce((obj, key) => Object.assign(obj, {[key]: argv[key]}), {});
+
 const config = Object.assign(Object.assign({}, pkg.build || {}), args)
 /**
  * Windows doesn't like executing `node_modules/.bin/build`
