d13327803d93150a55f4689a406729840174f4ab
Mark Wibrow <m.wibrow@gmail.com>
2020-05-24 14:45:44 +0100

    Fix module imports for dev/prod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,17 @@ import { InlineSVGModule } from "ng-inline-svg";
 import { CalvinApp } from "./app.component";
 
 import { ExampleWordPage } from "../pages/example-word/example-word";
+import { ExampleWordPageModule } from "../pages/example-word/example-word.module";
 import { HomePage } from "../pages/home/home";
+import { HomePageModule } from "../pages/home/home.module";
 import { SelectKeywordGroupPage } from "../pages/select-keyword-group/select-keyword-group";
+import { SelectKeywordGroupPageModule } from "../pages/select-keyword-group/select-keyword-group.module";
 import { SelectKeywordPage } from "../pages/select-keyword/select-keyword";
+import { SelectKeywordPageModule } from "../pages/select-keyword/select-keyword.module";
 import { SelectTalkerPage } from "../pages/select-talker/select-talker";
+import { SelectTalkerPageModule } from "../pages/select-talker/select-talker.module";
 import { VowelTrainerPage } from "../pages/vowel-trainer/vowel-trainer";
+import { VowelTrainerPageModule } from "../pages/vowel-trainer/vowel-trainer.module";
 
 import { AnimationFrameRequestProvider } from "../providers/animation-frame-request/animation-frame-request";
 import { AppDataProvider } from "../providers/app-data/app-data";
@@ -36,6 +42,12 @@ import { ComponentsModule } from "../components/components.module";
     InlineSVGModule,
     IonicModule.forRoot(CalvinApp),
     ComponentsModule,
+    HomePageModule,
+    SelectTalkerPageModule,
+    SelectKeywordGroupPageModule,
+    SelectKeywordPageModule,
+    ExampleWordPageModule,
+    VowelTrainerPageModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
