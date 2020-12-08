47c93ceb9c765c60e6030d29f4e1c29710c1a494
Mark Wibrow <m.wibrow@gmail.com>
2020-12-08 10:57:20 +0000

    Tidy up logo component

--- src/components/logo/logo.ts
+++ src/components/logo/logo.ts
@@ -1,19 +1,7 @@
 import { Component } from "@angular/core";
 
-/**
- * Generated class for the LogoComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
 @Component({
   selector: "logo",
   templateUrl: "logo.html",
 })
-export class LogoComponent {
-  text: string;
-
-  constructor() {
-    this.text = "Hello World";
-  }
-}
+export class LogoComponent {}
