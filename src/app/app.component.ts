88dc011fe1306daf17a6c2f1cb021fc47cf0588c
Mark Wibrow <m.wibrow@gmail.com>
2020-12-09 10:10:32 +0000

    Fix select talker page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,15 @@ import { Platform } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 
-// import { HomePage } from "../pages/home/home";
-import { SelectTalkerPage } from "../pages/select-talker/select-talker";
+import { HomePage } from "../pages/home/home";
 @Component({
   templateUrl: "app.html",
 })
 export class CalvinApp {
-  rootPage: any = SelectTalkerPage; // HomePage;
+  rootPage: any = HomePage;
   constructor(
     platform: Platform,
-    statusBar: StatusBar, 
+    statusBar: StatusBar,
     splashScreen: SplashScreen
   ) {
     platform.ready().then(() => {
